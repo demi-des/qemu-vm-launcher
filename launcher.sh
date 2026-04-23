4dff5a9221c83ef2ab7e54f7f5e8f05864fa7354
alessandro.ferraro <alessandro.ferraro@adelsrl.it>
2026-04-23 14:18:51 +0200

    refactor: Creating startup

---
+++ launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./resources/logo.sh
+
+echo "SELECT AN OPTION:"
+echo "1> Install / Update QEMU"
+echo "2> Create a virtual disk image"
+echo "3> Create and setup a WINDOWS virtual machine  ->  MUST create a virtual disk image first"
+echo "4> Boot a WINDOWS virtual machine already created"
+echo "5> Extend / Resize a virtual disk image"
